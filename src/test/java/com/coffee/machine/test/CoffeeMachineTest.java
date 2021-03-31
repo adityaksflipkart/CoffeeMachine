d1fa2154b903601be64dcf6e4143adf52b1a3758
Aditya Kumar Singh <aditya.ks@flipkart.com>
2021-03-31 23:46:02 +0530

    initial comit

--- src/test/java/com/coffee/machine/test/CoffeeMachineTest.java
+++ src/test/java/com/coffee/machine/test/CoffeeMachineTest.java
@@ -3,7 +3,6 @@ package com.coffee.machine.test;
 import com.coffee.machine.BeverageRequestStatus;
 import com.coffee.machine.CoffeeMachine;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -13,13 +12,10 @@ import java.util.concurrent.ExecutionException;
 
 public class CoffeeMachineTest {
     private CoffeeMachine coffeeMachine;
-    @Before
-    public void setup() throws IOException {
-        coffeeMachine = TestHelper.initTest(Constants.INPUT_FILE_PATH+"input.json");
-    }
 
     @Test
     public void allBeveragePrepared() throws IOException, ExecutionException, InterruptedException {
+        coffeeMachine = TestHelper.initTest(Constants.INPUT_FILE_PATH+"input.json");
         List<BeverageRequestStatus> beverageStatuses = coffeeMachine.prepareBeverage(Arrays.asList("hot_tea", "hot_coffee", "black_tea", "green_tea"));
         assert (beverageStatuses.isEmpty() == Boolean.FALSE);
         for (BeverageRequestStatus beverageStatus : beverageStatuses) {
@@ -28,7 +24,18 @@ public class CoffeeMachineTest {
     }
 
     @Test
+    public void noBeveragePrepared() throws IOException, ExecutionException, InterruptedException {
+        coffeeMachine = TestHelper.initTest(Constants.INPUT_FILE_PATH+"input2.json");
+        List<BeverageRequestStatus> beverageStatuses = coffeeMachine.prepareBeverage(Arrays.asList("hot_tea", "hot_coffee", "black_tea", "green_tea"));
+        assert (beverageStatuses.isEmpty() == Boolean.FALSE);
+        for (BeverageRequestStatus beverageStatus : beverageStatuses) {
+            assert (beverageStatus.isPrepared() == Boolean.FALSE);
+        }
+    }
+
+    @Test
     public void allBeveragePreparedMaterialNotAddedForOne() throws IOException, ExecutionException, InterruptedException {
+        coffeeMachine = TestHelper.initTest(Constants.INPUT_FILE_PATH+"input.json");
         List<BeverageRequestStatus> beverageStatuses = coffeeMachine.prepareBeverage(Arrays.asList("hot_tea", "hot_coffee", "black_tea", "green_tea","elaichi_tea"));
         assert (beverageStatuses.isEmpty() == Boolean.FALSE);
         for (BeverageRequestStatus beverageStatus : beverageStatuses) {
@@ -37,6 +44,25 @@ public class CoffeeMachineTest {
             }else
             assert (beverageStatus.isPrepared() == Boolean.TRUE);
         }
+    }
+
+    @Test
+    public void materialExhaustedAfterFewRequest() throws IOException, ExecutionException, InterruptedException {
+        coffeeMachine = TestHelper.initTest(Constants.INPUT_FILE_PATH+"input3.json");
+        List<BeverageRequestStatus> beverageStatuses = coffeeMachine.prepareBeverage(Arrays.asList("hot_tea", "hot_coffee", "black_tea", "green_tea"));
+        assert (beverageStatuses.isEmpty() == Boolean.FALSE);
+        int prepared =0;
+        int notPrepared =0;
+
+        for (BeverageRequestStatus beverageStatus : beverageStatuses) {
+            if(beverageStatus.isPrepared()){
+                prepared++;
+            }else{
+                notPrepared++;
+            }
+        }
+        assert (prepared >0);
+        assert (notPrepared >0);
     }
 
     @After
